ffdafcbc0cdd29509f927adf6df708aca1299feb
adler3d <adler3d@gmail.com>
2017-01-20 22:42:12 +0300

    Update utf8_to_cp1251.h

--- utf8_to_cp1251.h
+++ utf8_to_cp1251.h
@@ -4,8 +4,9 @@ static string utf8_to_cp1251(const string&s){
   auto*utf8=&s[0];
   vector<wchar_t> tmp(n,0);auto*wstr=&tmp[0];
   n=MultiByteToWideChar(CP_UTF8,0,utf8,n,wstr,n);
-  if(!n)return 0;
+  if(!n){auto ec=GetLastError();QapDebugMsg("ec="+IToS(ec));QapNoWay();return "";}
   string out(n,0);auto*s1251=&out[0];
-  WideCharToMultiByte(1251,0,wstr,n,s1251,n,NULL,NULL);
+  auto retval=WideCharToMultiByte(1251,0,wstr,n,s1251,n,NULL,NULL);
+  if(!retval){auto ec=GetLastError();QapDebugMsg("ec="+IToS(ec));QapNoWay();return "";}
   return out;
 }
