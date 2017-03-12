a1790844b9a81885e5364b96a143b0cc559eb0ad
adler3d <adler3d@gmail.com>
2017-03-12 13:30:52 +0300

    Update progressive_numbers.js

--- progressive_numbers.js
+++ progressive_numbers.js
@@ -1,6 +1,6 @@
 var arr=[0,1];
 for(var id=1;id<5;id++){
-  var n=Math.pow(2,id-1);var a0=0.5/n;var k=a0*2; 0.5/n+(1+j*2)
+  var n=Math.pow(2,id-1);var a0=0.5/n;var k=a0*2;// arr.push(a0+k*j)
   for(var j=0;j<n;j++){
     arr.push((1+j*2)+"/"+(n*2));
   }
