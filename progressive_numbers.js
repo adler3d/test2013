679ad2e95af311fc47cc6f4745a690cb256a1bd7
adler3d <adler3d@gmail.com>
2017-03-12 13:29:56 +0300

    Create progressive_numbers.js

---
+++ progressive_numbers.js
@@ -0,0 +1,8 @@
+var arr=[0,1];
+for(var id=1;id<5;id++){
+  var n=Math.pow(2,id-1);var a0=0.5/n;var k=a0*2; 0.5/n+(1+j*2)
+  for(var j=0;j<n;j++){
+    arr.push((1+j*2)+"/"+(n*2));
+  }
+}
+return inspect(arr./*sort().map(e=>e*16).*/join(" "));
