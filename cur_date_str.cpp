d15cb30ee51d5e8bea4d6bfb98fa7222315fed37
adler3d <adler3d@gmail.com>
2020-09-19 19:39:47 +0300

    Update cur_date_str.cpp

--- cur_date_str.cpp
+++ cur_date_str.cpp
@@ -57,23 +57,33 @@ static vector<string> split(const string&s,const string&needle){
   return arr;
 }
 
-static string conv_tm_to_str_v2(const std::tm&t){
-  auto*ptm=&t;
-  char buff[128];
-  sprintf(&buff[0],"%02u:%02u:%02u %04u.%02u.%02u\0",
-    ptm->tm_hour,ptm->tm_min,ptm->tm_sec,
-    1900+ptm->tm_year,
-    ptm->tm_mon+1,
-    ptm->tm_mday
-  );
-  return buff;
+static string local_cur_date_str_v3(){
+  FILETIME ft;GetSystemTimePreciseAsFileTime(&ft);
+  auto ts=ULARGE_INTEGER{ft.dwLowDateTime,ft.dwHighDateTime};
+  auto us=(ts.QuadPart)%(10*1000*1000);
+  SYSTEMTIME st;FileTimeToSystemTime(&ft,&st);
+  auto lt=st;GetLocalTime(&lt);
+  std::tm t={0};
+  #define F(A,B,C)t.A=lt.B+C;
+  F(tm_year,wYear,-1900);
+  F(tm_mon,wMonth,-1);
+  F(tm_wday,wDayOfWeek,0);
+  F(tm_mday,wDay,0);
+  F(tm_hour,wHour,0);
+  F(tm_min,wMinute,0);
+  F(tm_sec,wSecond,0);
+  #undef F
+  static const auto tm2str=[](const std::tm&t,int ms)->string{
+    auto*ptm=&t;char buff[128];
+    sprintf(&buff[0],"%02u:%02u:%02u.%03u %04u.%02u.%02u\0",
+      ptm->tm_hour,ptm->tm_min,ptm->tm_sec,ms,
+      1900+ptm->tm_year,ptm->tm_mon+1,ptm->tm_mday
+    );
+    return buff;
+  };
+  return tm2str(t,lt.wMilliseconds);
 }
-
-static string local_cur_date_str(){
-  time_t rawtime;time(&rawtime);
-  auto tm=*localtime(&rawtime);
-  return conv_tm_to_str_v2(tm);
-}
+auto timestamp=local_cur_date_str_v3;
 
 DWORD get_proc_id(HWND hwnd){DWORD dwProcId=0;GetWindowThreadProcessId(hwnd,&dwProcId);return dwProcId;}
 
@@ -116,7 +126,7 @@ void kb_type(string s){for(auto&c:s)kb_type_char(c);}
 static bool IsKeyDown(int vKey){int i=GetAsyncKeyState(vKey);return i<0;}
 
 void on_hot_key(){ 
-  auto s=local_cur_date_str();
+  auto s=timestamp();
   auto hwnd=GetForegroundWindow();
   string app_name=get_name_of_program_by_hwnd(s,hwnd);
   printf("[%s]: WM_HOTKEY received from app: %s\n",s.c_str(),app_name.c_str());
@@ -136,9 +146,9 @@ void winapi_set_hotkey_handler(...){
   size_t T='T';static const size_t B=0x42;static_assert('B'==B,"WTF?");
   size_t KEY=T;
   if(RegisterHotKey(NULL,1,MOD_ALT|MOD_NOREPEAT,KEY)){
-    printf("[%s]: begin\n",local_cur_date_str().c_str());
+    printf("[%s]: begin\n",timestamp().c_str());
   }else{
-    printf("[%s]: RegisterHotKey failed - look like hotkey ALT+T already taken by other program.\n",local_cur_date_str().c_str());
+    printf("[%s]: RegisterHotKey failed - look like hotkey ALT+T already taken by other program.\n",timestamp().c_str());
     return;
   }
  
@@ -152,8 +162,8 @@ void winapi_set_hotkey_handler(...){
 
 int main(){
   printf("cur_date_str.exe - app that insert local datetime when you press ALT+T inside notepad++.exe\n");
-  printf("[V4.6 build at 13:44:57 2020.09.19]\n");
-  printf("https://github.com/adler3d/test2013/blob/master/cur_date_str.cpp\n");
+  printf("[V5.0 build at 19:38:33.363 2020.09.19]\n");
+  printf("   https://github.com/adler3d/test2013/blob/master/cur_date_str.cpp\n");
   winapi_set_hotkey_handler("ALT+T",on_hot_key);
   return 0;
 }
