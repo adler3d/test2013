68770c81b3873da04496266df25fb7e9b5f4e3b7
adler3d <adler3d@gmail.com>
2020-09-20 01:08:52 +0300

    Update datetime.cpp

--- datetime.cpp
+++ datetime.cpp
@@ -21,6 +21,8 @@ cl /DUNICODE /D_UNICODE /O2 /LD datetime.cpp /nologo /EHsc
 #define NPPM_GETCURRENTSCINTILLA  (NPPMSG + 4)
 #define SCI_REPLACESEL 2170
 
+struct SCNotification{};
+
 const int nbChar = 64;
 
 typedef const TCHAR * (__cdecl * PFUNCGETNAME)();
@@ -31,12 +33,7 @@ struct NppData{
   HWND _scintillaSecondHandle;
 };
 
-struct SCNotification{};
-
-typedef void (__cdecl * PFUNCSETINFO)(NppData);
 typedef void (__cdecl * PFUNCPLUGINCMD)();
-typedef void (__cdecl * PBENOTIFIED)(SCNotification *);
-typedef LRESULT (__cdecl * PMESSAGEPROC)(UINT Message, WPARAM wParam, LPARAM lParam);
         
 struct ShortcutKey{
   bool _isCtrl;
@@ -44,6 +41,7 @@ struct ShortcutKey{
   bool _isShift;
   UCHAR _key;
 };
+ShortcutKey ALT_T={false,true,false,'T'};
 
 struct FuncItem{
   TCHAR _itemName[nbChar];
@@ -53,15 +51,23 @@ struct FuncItem{
   ShortcutKey *_pShKey;
 };
 
-typedef FuncItem * (__cdecl * PFUNCGETFUNCSARRAY)(int *);
-
 const int nbFunc = 1;
 extern FuncItem funcItem[nbFunc];
 extern NppData nppData;
 
 FuncItem funcItem[nbFunc];
 NppData nppData;
-     
+
+bool setCommand(size_t index, TCHAR *cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey *sk, bool check0nInit){
+  if(index>=nbFunc)return false;
+  if(!pFunc)return false;
+  lstrcpy(funcItem[index]._itemName, cmdName);
+  funcItem[index]._pFunc = pFunc;
+  funcItem[index]._init2Check = check0nInit;
+  funcItem[index]._pShKey = sk;
+  return true;
+}
+
 using std::string;
 static string local_cur_date_str_v3(){
   FILETIME ft;GetSystemTimePreciseAsFileTime(&ft);
@@ -84,28 +90,29 @@ static string local_cur_date_str_v3(){
   return buff;
 }
 
-void func_local_cur_date_str(){
+void insert_str_impl(const string&str){
   int which=-1;
   ::SendMessage(nppData._nppHandle,NPPM_GETCURRENTSCINTILLA,0,(LPARAM)&which);
   if(which==-1)return;
   HWND curScintilla = (which == 0)?nppData._scintillaMainHandle:nppData._scintillaSecondHandle;
 
-  auto s="---\n"+local_cur_date_str_v3()+"\n";
-  ::SendMessage(curScintilla, SCI_REPLACESEL, 0, (LPARAM)s.c_str());
-}
-    
-bool setCommand(size_t index, TCHAR *cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey *sk, bool check0nInit){
-  if(index>=nbFunc)return false;
-  if(!pFunc)return false;
-  lstrcpy(funcItem[index]._itemName, cmdName);
-  funcItem[index]._pFunc = pFunc;
-  funcItem[index]._init2Check = check0nInit;
-  funcItem[index]._pShKey = sk;
-  return true;
+  ::SendMessage(curScintilla, SCI_REPLACESEL, 0, (LPARAM)str.c_str());
 }
 
+void insert_datetime(){
+  auto s="---\n"+local_cur_date_str_v3()+"\n";
+  insert_str_impl(s);
+}
+
+void insert_link_to_source_code(){
+  auto s="---\n"+local_cur_date_str_v3()+"\n";
+  s+="https://github.com/adler3d/test2013/blob/master/datetime.cpp";
+  insert_str_impl(s);
+}
+    
 void commandMenuInit(){
-  setCommand(0, TEXT("insert_datetime"), func_local_cur_date_str, NULL, false);
+  setCommand(0, TEXT("insert_datetime"), insert_datetime, &ALT_T, false);
+  setCommand(0, TEXT("insert_link_to_source_code"), insert_link_to_source_code, NULL, false);
 }
 
 const TCHAR NPP_PLUGIN_NAME[] = TEXT("datetime");
